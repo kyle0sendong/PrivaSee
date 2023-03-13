432a49b869ac3d8040a8d45deb6271ed1a549149
Kyle Destura <kyle.afundar@gmail.com>
2023-03-13 14:20:04 +0800

    build updates

--- app/src/main/java/com/example/privasee/database/viewmodel/repository/UserRepository.kt
+++ app/src/main/java/com/example/privasee/database/viewmodel/repository/UserRepository.kt
@@ -4,20 +4,17 @@ import androidx.lifecycle.LiveData
 import com.example.privasee.database.viewmodel.repository.dao.UserDao
 import com.example.privasee.database.model.User
 
-class UserRepository (private val userDao: UserDao) {
+class UserRepository(private val userDao: UserDao) {
 
     val readAllData: LiveData<List<User>> = userDao.readAllData()
-
 
     suspend fun addUser(user: User) {
         userDao.addUser(user)
     }
 
-
     suspend fun updateUser(user: User) {
         userDao.updateUser(user)
     }
-
 
     suspend fun deleteUser(user: User) {
         userDao.deleteUser(user)
