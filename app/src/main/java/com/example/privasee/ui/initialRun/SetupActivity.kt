da1155e9308a39a0f97a3134e80cda15226bce4b
Kyle Destura <kyle.afundar@gmail.com>
2023-03-13 14:16:19 +0800

    Add first run setup

---
+++ app/src/main/java/com/example/privasee/ui/initialRun/SetupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.privasee.ui.initialRun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.privasee.R
+
+class SetupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup)
+    }
+}
