dd01588f3468ca32d7d1e022be3b76da3c7b5694
Kyle Destura <kyle.afundar@gmail.com>
2023-03-13 22:06:56 +0800

    Update database's App Model

--- app/src/main/java/com/example/privasee/database/model/App.kt
+++ app/src/main/java/com/example/privasee/database/model/App.kt
@@ -5,23 +5,8 @@ import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity (tableName = "app",
-         foreignKeys = [
-             ForeignKey(
-                entity = User::class,
-                parentColumns = ["id"],
-                childColumns = ["userId"],
-                onDelete = ForeignKey.CASCADE
-            )
-         ],
-        indices = [
-            Index(value = ["userId"])
-        ]
-)
-
+@Entity (tableName = "app")
 data class App(
     @PrimaryKey val packageName : String,
     val appName : String,
-    val restrict : Boolean,
-    val userId : Int
 )
