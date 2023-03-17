88e224e4e4b782da0fbc54d01d473770ddff1d7b
Kyle Destura <kyle.afundar@gmail.com>
2023-03-17 17:14:04 +0800

    Add initial codes for AppMonitoring

---
+++ app/src/main/java/com/example/privasee/ui/appMonitoring/AppMonitoringActivity.kt
@@ -0,0 +1,12 @@
+package com.example.privasee.ui.appMonitoring
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.privasee.R
+
+class AppMonitoringActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_monitoring)
+    }
+}
--- app/src/main/java/com/example/privasee/ui/initialRun/SetupOwnerFragment.kt
+++ app/src/main/java/com/example/privasee/ui/initialRun/SetupOwnerFragment.kt
@@ -1,5 +1,6 @@
 package com.example.privasee.ui.initialRun
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.privasee.R
 import com.example.privasee.databinding.FragmentSetupOwnerBinding
+import com.example.privasee.ui.appMonitoring.AppMonitoringActivity
+import com.example.privasee.ui.appRestriction.AppRestrictionActivity
 
 
 class SetupOwnerFragment : Fragment() {
@@ -20,6 +23,12 @@ class SetupOwnerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSetupOwnerBinding.inflate(inflater, container, false)
+
+        binding.btnSelectApps.setOnClickListener {
+            Intent(requireContext(), AppMonitoringActivity::class.java).also {
+                startActivity(it)
+            }
+        }
 
         binding.btnSetupOwnerFinish.setOnClickListener {
             findNavController().navigate(R.id.action_setupOwnerFragment_to_mainActivity)
