4e5c7a587f0bbf75b51d589f4652da967957ce1b
Kyle Destura <kyle.afundar@gmail.com>
2023-03-25 15:27:34 +0800

    Update UI and setup navhost

--- app/src/main/java/com/example/privasee/ui/user/userSettings/userAppControl/UserAppControllingActivity.kt
+++ app/src/main/java/com/example/privasee/ui/user/userSettings/userAppControl/UserAppControllingActivity.kt
@@ -2,17 +2,25 @@ package com.example.privasee.ui.user.userSettings.userAppControl
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.privasee.R
 import com.example.privasee.databinding.ActivityUserAppControllingBinding
 
 class UserAppControllingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUserAppControllingBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityUserAppControllingBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcvControlling) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
 
     }
 }
