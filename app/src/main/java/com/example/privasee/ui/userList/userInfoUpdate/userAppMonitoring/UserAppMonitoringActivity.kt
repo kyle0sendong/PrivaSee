fd280f9e3f4f7dd2965094b8aa12ebd83bc5b8f1
Kyle Destura <kyle.afundar@gmail.com>
2023-03-27 14:16:41 +0800

    Removed restrictviewmodel

--- app/src/main/java/com/example/privasee/ui/userList/userInfoUpdate/userAppMonitoring/UserAppMonitoringActivity.kt
+++ app/src/main/java/com/example/privasee/ui/userList/userInfoUpdate/userAppMonitoring/UserAppMonitoringActivity.kt
@@ -2,19 +2,17 @@ package com.example.privasee.ui.userList.userInfoUpdate.userAppMonitoring
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.privasee.R
-import com.example.privasee.database.viewmodel.RestrictionViewModel
 import com.example.privasee.databinding.ActivityUserAppMonitoringBinding
 
 class UserAppMonitoringActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUserAppMonitoringBinding
     private lateinit var navController: NavController
-    private lateinit var mRestrictionViewModel: RestrictionViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUserAppMonitoringBinding.inflate(layoutInflater)
@@ -31,7 +29,6 @@ class UserAppMonitoringActivity : AppCompatActivity() {
             bundle.putInt("userId", userId)
 
         navController.setGraph(R.navigation.monitoring_nav, bundle)
-        mRestrictionViewModel = ViewModelProvider(this)[RestrictionViewModel::class.java]
 
     }
 
