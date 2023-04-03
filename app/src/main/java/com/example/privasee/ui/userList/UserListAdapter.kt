e22b63fca1e5e6ae82705b8109b0cfb64899e12f
Kyle Destura <kyle.afundar@gmail.com>
2023-04-03 13:14:17 +0800

    Update builds

--- app/src/main/java/com/example/privasee/ui/userList/UserListAdapter.kt
+++ app/src/main/java/com/example/privasee/ui/userList/UserListAdapter.kt
@@ -1,18 +1,26 @@
 package com.example.privasee.ui.userList
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.privasee.database.model.User
+import com.example.privasee.database.viewmodel.RestrictionViewModel
 import com.example.privasee.databinding.RecyclerItemUserBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class UserListAdapter(): RecyclerView.Adapter<UserListAdapter.UserViewHolder>() {
 
     inner class UserViewHolder(val binding: RecyclerItemUserBinding): RecyclerView.ViewHolder(binding.root)
     private var userList = emptyList<User>()
-
+    private lateinit var mRestrictionViewModel: RestrictionViewModel
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = RecyclerItemUserBinding.inflate(layoutInflater, parent, false)
@@ -24,7 +32,14 @@ class UserListAdapter(): RecyclerView.Adapter<UserListAdapter.UserViewHolder>() 
 
         holder.binding.apply {
             tvUserName.text = currentUser.name
+            val owner = "Owner"
 
+            if (currentUser.isOwner)
+                tvIsOwner.text = owner
+            else
+                tvIsOwner.isVisible = false
+
+            Log.d("tagimandos", "userlistadapter $currentUser")
             recyclerItemUser.setOnClickListener {
                 val action = UserListFragmentDirections.actionUserFragmentToUpdateUserFragment(currentUser)
                 recyclerItemUser.findNavController().navigate(action)
