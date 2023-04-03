5517b831cead6ed12a7c43e92f1e3a4e28270cef
Kyle Destura <kyle.afundar@gmail.com>
2023-04-03 13:12:08 +0800

    Remove checking of accesibility service on monitor fragment

--- app/src/main/java/com/example/privasee/ui/monitorAccess/MonitorAccessFragment.kt
+++ app/src/main/java/com/example/privasee/ui/monitorAccess/MonitorAccessFragment.kt
@@ -5,12 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.privasee.databinding.FragmentMonitorAccessBinding
 import com.example.privasee.ui.userList.userInfoUpdate.userAppControl.UserAppControllingActivity
 import com.example.privasee.ui.userList.userInfoUpdate.userAppMonitoring.UserAppMonitoringActivity
-import com.example.privasee.utils.CheckPermissionUtils
 
 class MonitorAccessFragment : Fragment() {
 
@@ -22,7 +20,7 @@ class MonitorAccessFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMonitorAccessBinding.inflate(inflater, container, false)
-        CheckPermissionUtils.checkAccessibilityPermission(requireContext())
+
         return binding.root
     }
 
